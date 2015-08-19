0e441bd42f53a8fd4904f1ae98254177cab4b570
msawka <matt.sawka@perceptivesoftware.com>
2015-08-19 09:48:15 -0500

    update dependencies to latest

--- mix.exs
+++ mix.exs
@@ -40,17 +40,17 @@ defmodule OpenAperture.FleetManager.Mixfile do
       {:ex_doc, github: "elixir-lang/ex_doc", only: [:test], override: true},
       {:earmark, github: "pragdave/earmark", tag: "v0.1.8", only: [:test], override: true},    
       
-      {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "abb5a54cabdc9c70a3d8e22558ee9c7c227c3c22", override: true},
-      {:openaperture_manager_api, git: "https://github.com/OpenAperture/manager_api.git", ref: "ab5334f276b308706a91e85ca27ba937bb02fb9f", override: true},
-      {:openaperture_overseer_api, git: "https://github.com/OpenAperture/overseer_api.git", ref: "25c779ea50565cdb3f783cba644294e6238ed72a", override: true},
+      {:openaperture_messaging, git: "https://github.com/OpenAperture/messaging.git", ref: "3d3a84eabf4ba0a3a827a61c4d99cdbf0ab49a0d", override: true},
+      {:openaperture_manager_api, git: "https://github.com/OpenAperture/manager_api.git", ref: "86cf2c324434f9899416881219e03c0f959c2896", override: true},
+      {:openaperture_overseer_api, git: "https://github.com/OpenAperture/overseer_api.git", ref: "4b9146507ab50789fec4696b96f79642add2b502", override: true},
       {:openaperture_fleet, git: "https://github.com/OpenAperture/fleet.git", ref: "324acdae0ceecb6a954d804d56d9d2fceaeb937c", override: true},
-      {:timex, "~> 0.12.9"},
+      {:timex, "~> 0.13.3", override: true},
       {:fleet_api, "~> 0.0.15", override: true},
       {:poison, "~> 1.4.0", override: true},
 
       #test dependencies
       {:exvcr, github: "parroty/exvcr", only: :test},
-      {:meck, "0.8.2", only: :test}
+      {:meck, "0.8.3", override: true},
     ]
   end
 end
